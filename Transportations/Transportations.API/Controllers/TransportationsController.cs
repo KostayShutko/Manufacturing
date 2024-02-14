133ef0eace5d171967d429df8a4c9e77e3c84035
DESKTOP-7H1RKU4\Konstantin <shoker519@mail.ru>
2024-02-14 20:31:46 +0100

    check MaterialsWarehouse cicd. changed Transportations

--- Transportations/Transportations.API/Controllers/TransportationsController.cs
+++ Transportations/Transportations.API/Controllers/TransportationsController.cs
@@ -15,7 +15,7 @@ public class TransportationsController : ControllerBase
 
     public TransportationsController(IMediator mediator)
     {
-        this.mediator = mediator;
+        this.mediator = mediator;//com
     }
 
     [HttpGet]
